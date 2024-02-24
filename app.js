2b56eac3c88aa1579e81bc6ac8df6ec12317111c
amiradagdoug <amiradagdoug@yahoo.fr>
2024-02-24 23:39:41 +0100

    cicdpipeline30014

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World amira dag!')
+    res.send('Hello World amira2 dag!')
 })
 
 app.listen(port, () => {
